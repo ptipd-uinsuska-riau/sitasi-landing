82cd92c11045b383671822728730ad22f2db672d
Erno Irwandi <49245597+waeerno@users.noreply.github.com>
2023-12-21 22:19:19 +0700

    ekosistem

--- Ecosystem.config.js
+++ Ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             exec_mode: "cluster",
             port: 5000,
             instances: "max", // sesuaikan dengan core atau bisa isi max untuk menggunakan semua core
-            script: ".output/server/index.mjs",
+            script: "./.output/server/index.mjs",
         },
     ],
 };
