101e28888493d23239c3f81101ff67f849a54b1f
Erno Irwandi <49245597+waeerno@users.noreply.github.com>
2023-12-21 21:06:26 +0700

    ekosistem

--- Ecosystem.config.js
+++ Ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    apps: [{
-        name: 'sitasi-landing',
-        exec_mode: 'cluster',
-        instances: 'max', // sesuaikan dengan core atau bisa isi max untuk menggunakan semua core
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-    }]
-}
+    apps: [
+        {
+            name: "sitasi-landing",
+            exec_mode: "cluster",
+            port: 5000,
+            instances: "max", // sesuaikan dengan core atau bisa isi max untuk menggunakan semua core
+            script: "./.output/server/index.mjs",
+        },
+    ],
+};
